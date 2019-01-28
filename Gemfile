ec272d9a360e8ae1412e86df33abceb26e2ed275
sabud1208 <43481963+sabud1208@users.noreply.github.com>
2019-01-28 10:15:03 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -9,7 +9,8 @@ gem 'sinatra'
     gem 'shotgun'
     gem 'pry'
     gem 'require_all'
-    
+    gem 'nokogiri'
+
 group :test do
       gem 'rspec'
       gem 'capybara'
--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+  get '/nothing' do
+  status 200
+  body ''
+end
 
+   get '/'  do
+     'Hello, World!'
+   end
 
 end
